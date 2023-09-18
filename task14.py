026cf0095b36af5659ba870372cc5d7b2fddcb2a
Daniil0114 <danich.sh.cher01@gmail.com>
2023-09-18 21:07:41 +0300

    task14.py

--- task14.py
+++ task14.py
@@ -1,5 +1,6 @@
 #Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k),# не превосходящие числа N.
 n_digit = int(input("Введите значение N: "))
-digit = 1while digit < n_digit:
+digit = 1
+while digit < n_digit:
     print(digit, end=',')
     digit = digit * 2
